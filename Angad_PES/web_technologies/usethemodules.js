2982057786689bb4cc855fc500ba321c89eeab87
Anga205 <angadbhalla2005@gmail.com>
2024-10-16 09:03:38 +0530

    added usermodules code

---
+++ Angad_PES/web_technologies/usethemodules.js
@@ -0,0 +1,5 @@
+var calc = require('./usermodule.js');
+console.log("Addition: ",calc.add(2,3));
+console.log("Subtraction: ",calc.sub(2,3));
+console.log("Multiplication: ",calc.mul(2,3));
+console.log("Division: ",calc.div(2,3));
